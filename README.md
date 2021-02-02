4f78550040ea8a95cc96c4cb9216681fdcef4842
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2021-02-02 15:47:27 +0100

    Add a route for getting traffic

---
+++ README.md
@@ -0,0 +1,16 @@
+# Running dev mode
+
+I made Make tasks for venv/pip/flask
+
+```shell
+$ make install
+$ make dev
+```
+
+# Running prod/persistently
+
+I recommend Docker Compose
+
+```shell
+$ docker-compose up -d
+```
--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from pyvrt import weather
+from pyvrt import weather, traffic
 from pyvrt.media import stubru
 
 app = Flask(__name__, instance_relative_config=True)
@@ -16,6 +16,15 @@ def get_weather():
 def get_stubru():
     c = stubru.current()
     return c
+
+
+@app.route('/traffic')
+def get_traffic():
+    t = traffic.traffic_teaser()
+    j = traffic.traffic_jam(km=True)
+
+    t["teaser"]["trafficJamLength"] = j["length"]
+    return t
 
 
 if __name__ == "main":
